e0cb048209ccaf90046e1c292d2b7e41d954a6a8
Payal Lenka <payalm.lenka@gmail.com>
2024-07-29 00:51:33 +0530

    Update admin.py

--- obeseApp/admin.py
+++ obeseApp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import FormData  # Import your model here
-
-@admin.register(FormData)
+from .models import MyFormData  
+@admin.register(MyFormData)
 class FormDataAdmin(admin.ModelAdmin):
     list_display = ('age', 'gender', 'location', 'occupation', 'encounter_obese', 'profession_of_obese', 'primary_causes_of_obesity', 'junk_food_main_cause', 'used_fitness_app', 'fitness_apps_used', 'reasons_for_app_failure', 'effective_features', 'barriers_to_adherence', 'additional_comments')
     search_fields = ('location', 'occupation', 'profession_of_obese', 'additional_comments')
